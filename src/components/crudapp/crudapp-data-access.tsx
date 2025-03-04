ef02b73d33218123294e6b1276e1cba0c08cc83d
JKrishnaD <jayak5063@gmail.com>
2025-03-04 22:53:58 +0530

    dapp ui

--- src/components/crudapp/crudapp-data-access.tsx
+++ src/components/crudapp/crudapp-data-access.tsx
@@ -1,104 +1,107 @@
-'use client'
+"use client";
 
-import { getCrudappProgram, getCrudappProgramId } from '@project/anchor'
-import { useConnection } from '@solana/wallet-adapter-react'
-import { Cluster, Keypair, PublicKey } from '@solana/web3.js'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { useMemo } from 'react'
-import toast from 'react-hot-toast'
-import { useCluster } from '../cluster/cluster-data-access'
-import { useAnchorProvider } from '../solana/solana-provider'
-import { useTransactionToast } from '../ui/ui-layout'
+import { getCrudappProgram, getCrudappProgramId } from "@project/anchor";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { Cluster, Keypair, PublicKey } from "@solana/web3.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useCluster } from "../cluster/cluster-data-access";
+import { useAnchorProvider } from "../solana/solana-provider";
+import { useTransactionToast } from "../ui/ui-layout";
+import toast from "react-hot-toast";
+
+interface CreateTodoArgs {
+  title: string;
+  body: string;
+  is_done: boolean;
+  owner: PublicKey;
+}
 
 export function useCrudappProgram() {
-  const { connection } = useConnection()
-  const { cluster } = useCluster()
-  const transactionToast = useTransactionToast()
-  const provider = useAnchorProvider()
-  const programId = useMemo(() => getCrudappProgramId(cluster.network as Cluster), [cluster])
-  const program = useMemo(() => getCrudappProgram(provider, programId), [provider, programId])
+  const { connection } = useConnection();
+  const { cluster } = useCluster();
+  const transactionToast = useTransactionToast();
+  const provider = useAnchorProvider();
+  const programId = useMemo(
+    () => getCrudappProgramId(cluster.network as Cluster),
+    [cluster]
+  );
+  const program = useMemo(
+    () => getCrudappProgram(provider, programId),
+    [provider, programId]
+  );
 
   const accounts = useQuery({
-    queryKey: ['crudapp', 'all', { cluster }],
-    queryFn: () => program.account.crudapp.all(),
-  })
+    queryKey: ["crudapp", "all", { cluster }],
+    queryFn: () => program.account.todoEntry.all(),
+  });
 
   const getProgramAccount = useQuery({
-    queryKey: ['get-program-account', { cluster }],
+    queryKey: ["get-program-account", { cluster }],
     queryFn: () => connection.getParsedAccountInfo(programId),
-  })
+  });
 
-  const initialize = useMutation({
-    mutationKey: ['crudapp', 'initialize', { cluster }],
-    mutationFn: (keypair: Keypair) =>
-      program.methods.initialize().accounts({ crudapp: keypair.publicKey }).signers([keypair]).rpc(),
-    onSuccess: (signature) => {
-      transactionToast(signature)
-      return accounts.refetch()
+  const createTodo = useMutation<string, Error, CreateTodoArgs>({
+    mutationKey: [`todoEntry`, `create`, { cluster }],
+    mutationFn: async ({ title, body, is_done, owner }) => {
+      return program.methods.createTodo(title, body, is_done).rpc();
     },
-    onError: () => toast.error('Failed to initialize account'),
-  })
+    onSuccess: (signature) => {
+      transactionToast(signature);
+    },
+    onError: (error) => {
+      toast.error(`Error: ${error.message}`);
+    },
+  });
 
   return {
     program,
     programId,
     accounts,
     getProgramAccount,
-    initialize,
-  }
+    createTodo,
+  };
 }
 
 export function useCrudappProgramAccount({ account }: { account: PublicKey }) {
-  const { cluster } = useCluster()
-  const transactionToast = useTransactionToast()
-  const { program, accounts } = useCrudappProgram()
+  const { cluster } = useCluster();
+  const transactionToast = useTransactionToast();
+  const { program, accounts } = useCrudappProgram();
 
   const accountQuery = useQuery({
-    queryKey: ['crudapp', 'fetch', { cluster, account }],
-    queryFn: () => program.account.crudapp.fetch(account),
-  })
+    queryKey: ["crudapp", "fetch", { cluster, account }],
+    queryFn: () => program.account.todoEntry.fetch(account),
+  });
 
-  const closeMutation = useMutation({
-    mutationKey: ['crudapp', 'close', { cluster, account }],
-    mutationFn: () => program.methods.close().accounts({ crudapp: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accounts.refetch()
+  const updateTodo = useMutation<string, Error, CreateTodoArgs>({
+    mutationKey: [`todoEntry`, `update`, { cluster }],
+    mutationFn: async ({ title, body, is_done }) => {
+      return program.methods.updateTodo(title, body, is_done).rpc();
     },
-  })
+    onSuccess: (signature) => {
+      transactionToast(signature);
+      accounts.refetch();
+    },
+    onError: (error) => {
+      toast.error(`Error: ${error.message}`);
+    },
+  });
 
-  const decrementMutation = useMutation({
-    mutationKey: ['crudapp', 'decrement', { cluster, account }],
-    mutationFn: () => program.methods.decrement().accounts({ crudapp: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
+  const deleteTodo = useMutation({
+    mutationKey: [`todoEntry`, `delete`, { cluster }],
+    mutationFn: () => {
+      return program.methods.deleteTodo().rpc();
     },
-  })
 
-  const incrementMutation = useMutation({
-    mutationKey: ['crudapp', 'increment', { cluster, account }],
-    mutationFn: () => program.methods.increment().accounts({ crudapp: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
+    onSuccess: (signature) => {
+      transactionToast(signature);
+      accounts.refetch();
     },
-  })
-
-  const setMutation = useMutation({
-    mutationKey: ['crudapp', 'set', { cluster, account }],
-    mutationFn: (value: number) => program.methods.set(value).accounts({ crudapp: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
-    },
-  })
+  });
 
   return {
     accountQuery,
-    closeMutation,
-    decrementMutation,
-    incrementMutation,
-    setMutation,
-  }
+    updateTodo,
+    deleteTodo,
+  };
 }
--- src/components/crudapp/crudapp-ui.tsx
+++ src/components/crudapp/crudapp-ui.tsx
@@ -1,122 +1,172 @@
-'use client'
+"use client";
 
-import { Keypair, PublicKey } from '@solana/web3.js'
-import { useMemo } from 'react'
-import { ellipsify } from '../ui/ui-layout'
-import { ExplorerLink } from '../cluster/cluster-ui'
-import { useCrudappProgram, useCrudappProgramAccount } from './crudapp-data-access'
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { useMemo, useState } from "react";
+import { ellipsify } from "../ui/ui-layout";
+import { ExplorerLink } from "../cluster/cluster-ui";
+import {
+  useCrudappProgram,
+  useCrudappProgramAccount,
+} from "./crudapp-data-access";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export function CrudappCreate() {
-  const { initialize } = useCrudappProgram()
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [isDone, setIsDone] = useState(false);
+  const { createTodo } = useCrudappProgram();
+  const { publicKey } = useWallet();
+
+  const isVerified = title.trim() && body.trim() != "";
+
+  const onSubmit = () => {
+    if (publicKey && isVerified) {
+      createTodo.mutateAsync({
+        title,
+        body,
+        is_done: isDone,
+        owner: publicKey as PublicKey,
+      });
+    }
+  };
+
+  if (!publicKey) {
+    return <p>Connect your wallet</p>;
+  }
 
   return (
-    <button
-      className="btn btn-xs lg:btn-md btn-primary"
-      onClick={() => initialize.mutateAsync(Keypair.generate())}
-      disabled={initialize.isPending}
-    >
-      Create {initialize.isPending && '...'}
-    </button>
-  )
+    <div className="flex w-full max-w-4xl space-y-4 items-center justify-center flex-col">
+      <input
+        type="text"
+        placeholder="todo title"
+        onChange={(e) => setTitle(e.target.value)}
+        className="input input-bordered w-full max-w-2xl"
+      />
+      <textarea
+        placeholder="todo description"
+        onChange={(e) => setBody(e.target.value)}
+        className="textarea textarea-bordered w-full max-w-2xl"
+      />
+      <button className="flex flex-row space-x-2">
+        <input type="checkbox" onChange={(e) => setIsDone(e.target.checked)} />
+        <span>Mark as completed</span>
+      </button>
+      <button
+        onClick={onSubmit}
+        disabled={!isVerified}
+        className="btn btn-primary"
+      >
+        Create Todo
+      </button>
+    </div>
+  );
 }
 
 export function CrudappList() {
-  const { accounts, getProgramAccount } = useCrudappProgram()
+  const { accounts, getProgramAccount } = useCrudappProgram();
 
   if (getProgramAccount.isLoading) {
-    return <span className="loading loading-spinner loading-lg"></span>
+    return <span className="loading loading-spinner loading-lg"></span>;
   }
   if (!getProgramAccount.data?.value) {
     return (
       <div className="alert alert-info flex justify-center">
-        <span>Program account not found. Make sure you have deployed the program and are on the correct cluster.</span>
+        <span>
+          Program account not found. Make sure you have deployed the program and
+          are on the correct cluster.
+        </span>
       </div>
-    )
+    );
   }
   return (
-    <div className={'space-y-6'}>
+    <div className={"space-y-6"}>
       {accounts.isLoading ? (
         <span className="loading loading-spinner loading-lg"></span>
       ) : accounts.data?.length ? (
         <div className="grid md:grid-cols-2 gap-4">
           {accounts.data?.map((account) => (
-            <CrudappCard key={account.publicKey.toString()} account={account.publicKey} />
+            <CrudappCard
+              key={account.publicKey.toString()}
+              account={account.publicKey}
+            />
           ))}
         </div>
       ) : (
         <div className="text-center">
-          <h2 className={'text-2xl'}>No accounts</h2>
+          <h2 className={"text-2xl"}>No accounts</h2>
           No accounts found. Create one above to get started.
         </div>
       )}
     </div>
-  )
+  );
 }
 
 function CrudappCard({ account }: { account: PublicKey }) {
-  const { accountQuery, incrementMutation, setMutation, decrementMutation, closeMutation } = useCrudappProgramAccount({
+  const { accountQuery, updateTodo, deleteTodo } = useCrudappProgramAccount({
     account,
-  })
+  });
 
-  const count = useMemo(() => accountQuery.data?.count ?? 0, [accountQuery.data?.count])
+  const { publicKey } = useWallet();
+  const title = accountQuery.data?.title;
+  const [body, setBody] = useState("");
+  const [isDone, setIsDone] = useState(false);
+
+  const isValid = body.trim() != "";
+
+  if (!publicKey) {
+    return <p>connect your wallet</p>;
+  }
+
+  if (!title) {
+    return <p>failed to fetch the title from account...</p>;
+  }
+
+  const handleUpdate = () => {
+    if (publicKey && isValid) {
+      updateTodo.mutateAsync({
+        title,
+        body,
+        is_done: isDone,
+        owner: publicKey,
+      });
+    }
+  };
 
   return accountQuery.isLoading ? (
-    <span className="loading loading-spinner loading-lg"></span>
+    <span className="loading-spinner loading loading-lg" />
   ) : (
     <div className="card card-bordered border-base-300 border-4 text-neutral-content">
-      <div className="card-body items-center text-center">
-        <div className="space-y-6">
-          <h2 className="card-title justify-center text-3xl cursor-pointer" onClick={() => accountQuery.refetch()}>
-            {count}
-          </h2>
-          <div className="card-actions justify-around">
-            <button
-              className="btn btn-xs lg:btn-md btn-outline"
-              onClick={() => incrementMutation.mutateAsync()}
-              disabled={incrementMutation.isPending}
-            >
-              Increment
-            </button>
-            <button
-              className="btn btn-xs lg:btn-md btn-outline"
-              onClick={() => {
-                const value = window.prompt('Set value to:', count.toString() ?? '0')
-                if (!value || parseInt(value) === count || isNaN(parseInt(value))) {
-                  return
-                }
-                return setMutation.mutateAsync(parseInt(value))
-              }}
-              disabled={setMutation.isPending}
-            >
-              Set
-            </button>
-            <button
-              className="btn btn-xs lg:btn-md btn-outline"
-              onClick={() => decrementMutation.mutateAsync()}
-              disabled={decrementMutation.isPending}
-            >
-              Decrement
-            </button>
-          </div>
-          <div className="text-center space-y-4">
-            <p>
-              <ExplorerLink path={`account/${account}`} label={ellipsify(account.toString())} />
-            </p>
-            <button
-              className="btn btn-xs btn-secondary btn-outline"
-              onClick={() => {
-                if (!window.confirm('Are you sure you want to close this account?')) {
-                  return
-                }
-                return closeMutation.mutateAsync()
-              }}
-              disabled={closeMutation.isPending}
-            >
-              Close
-            </button>
+      <div className="card card-body flex items-center flex-col text-center">
+        <div className="space-y-4">
+          <h1 onClick={() => accountQuery.refetch()} className="cursor-pointer">
+            {accountQuery.data?.title}
+          </h1>
+          <p>{accountQuery.data?.body}</p>
+          <div className="space-y-4 card-actions justify-around">
+            <textarea
+              placeholder="todo description"
+              onChange={(e) => setBody(e.target.value)}
+              className="textarea textarea-bordered w-full max-w-2xl"
+            />
+            <div className="flex flex-row">
+              <button
+                onClick={handleUpdate}
+                disabled={updateTodo.isPending || !isValid}
+                className="btn btn-xs btn-primary lg:btn-md"
+              >
+                Update Todo
+              </button>
+              <button
+                onClick={() => deleteTodo.mutate()}
+                disabled={deleteTodo.isPending}
+                className="btn btn-xs btn-primary lg:btn-md"
+              >
+                Delete Todo
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
